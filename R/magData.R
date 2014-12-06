cc80bfbfa7edd5318a5808b697b7298fa3411155
Carl Lipo <clipo@csulb.edu>
2014-12-05 21:23:01 -0800

    some initial scripting -- now trimming data and getting MUCH metter results.

--- R/magData.R
+++ R/magData.R
@@ -1,14 +1,16 @@
-
+library(sp)
 library(classInt)
 library(raster)
 library(rasterVis)
 library(colorspace)
 library(akima)
+library(rpanel)
+install.packages("rgeos", repos="http://R-Forge.R-project.org")
 install.packages('rgdal', type='source') 
 # dont forget to "brew install gdal --HEAD"
 # Download and install GDAL OS X install from kyngchaos
 #  http://www.kyngchaos.com/software/frameworks)
-# export PATH=”/Library/Frameworks/GDAL.framework/Programs:$PATH”
+  # export PATH=”/Library/Frameworks/GDAL.framework/Programs:$PATH”
 
 #2. Download and install proj4 from source
 #- http://trac.osgeo.org/proj/wiki/WikiStart#Download
@@ -27,8 +29,40 @@ install.packages('rgdal', type='source')
 setwd("/Volumes/Macintosh HD/Users/clipo/Dropbox/NAGPRA (2)/Reburial Project/MagData")
 data.path <- "attachment.ashx.dat" 
 
+showPal <- function(pal, labs=pal, cex=0.6, ...){
+  barplot(rep(1, length(pal)), col=pal,
+          names.arg=labs, cex.names=cex,
+          axes=FALSE, ...)
+}
+divPAL=brewer.pal(11,"RdYlBu")[11:1]
+
+break2pal <- function(x, mx, pal){
+  ## x = mx gives y = 1
+  ## x = 0 gives y = 0.5
+  y <- 1/2*(x/mx + 1)
+  rgb(pal(y), maxColorValue=255)
+}
+
+library(outliers)
+trimData <- function(x) {
+  limits<-quantile(x,  c(0.1,0.9))
+  #This finds the location of the outlier by finding that "True" value within the "outlier_tf" array. 
+  data_new <- x[which(x>limits[1] & x<limits[2])]
+  #This creates a new dataset based on the old data, removing the one row that contains the outlier 
+  data_new
+}
+
 # We read the whole file
 mag <- read.table(data.path, header = FALSE, sep="\t")
+magData <- mag[,6]
+mdensity <- density(magData)
+plot(mdensity)
+polygon(mdensity, col="red")
+
+trimMagData <- trimData(magData)
+tmdensity <- density(trimMagData)
+plot(tmdensity)
+polygon(tmdensity, col="red")
 
 class_e=classIntervals(mag[,6],30,style="equal")
 class_q=classIntervals(mag[,6],30,style="quantile")
@@ -37,6 +71,7 @@ class_km=classIntervals(mag[,6],30,style="kmeans")
 class_hc=classIntervals(mag[,6],30,style="hclust")
 class_f=classIntervals(mag[,6],30,style="fisher")
 pal1 <- c("wheat1", "red3")
+plotclr=colorRampPalette(brewer.pal(7,"RdYlBu")[7:1] )(200)
 plot(class_km, pal=pal1, main="fishers")
 
 myTheme=rasterTheme(region=sequential_hcl(10, power=2.2))
@@ -47,23 +82,25 @@ ymn=min(data1[,2]); ymx=max(data1[,2])
 r <- raster(nrows=100, ncols=100, 
             xmn=xmn, xmx=xmx, 
             ymn=ymn, ymx=ymx )
-ras_raw <- rasterize(data1[,1:2], r, field = data1[,3])
+ras_raw <- rasterize(data1[,1:2], r, field = trimMagData)
 
 # make a plot of the data
 plot(ras_raw)
+
 #levelplot(mag, par.settings=myTheme, contour=TRUE)
 library(RColorBrewer)
 plotclr=colorRampPalette(brewer.pal(7,"RdYlBu")[7:1] )(200)
-colcode=findColours(class_e, plotclr)  
+colcode=findColours(class_sd, plotclr)  
 plot(ras_raw,col=colcode)
 
 colcode=findColours(class_sd, plotclr)  
 plot(ras_raw,col=colcode)
 
-colcode=findColours(class_km, plotclr)  
+colcode=findColours(class_sd, plotclr)  
 plot(ras_raw,col=colcode)
 plotclr <- rev(brewer.pal(11, "Spectral"))
-colcode=findColours(class_q, plotclr)  
+divPAL <-brewer.pal(n=11,'PuOr')
+colcode=findColours(class_sd, divPAL)  
 plot(ras_raw,col=colcode)
 
 
@@ -82,3 +119,15 @@ image(akima.li, add=TRUE, col = rainbow(100, alpha = 1))
 contour(akima.li, add=TRUE)
 # plot data collection points over the top
 points(data1[,1], data1[,2], pch = 3, cex = 0.25)
+
+divPAL <-brewer.pal(n=11,'Spectral')
+divPAL[6]<-"#FFFFFF"
+showPal(divPAL)
+divTheme <- rasterTheme(region=divPAL)
+levelplot(ras_raw, contour=TRUE, par.settings=divTheme)
+
+grid <- expand.grid(x=mag[,1], y=mag[,2])
+
+levelplot(mag[,3]~mag[,1]*mag[,2], grid, cuts = 50, scales=list(log="e"), xlab="",
+          ylab="", main="Weird Function", sub="with log scales",
+          colorkey = FALSE, region = TRUE)
